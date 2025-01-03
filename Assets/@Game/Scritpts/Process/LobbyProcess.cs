5c109478b7d5fdc5bb050bef410d8f80c0a442aa
Thon <wooson0308@gmail.com>
2025-01-03 17:19:24 +0900

    팔로우 플레이어 카메라

--- Assets/@Game/Scritpts/Process/LobbyProcess.cs
+++ Assets/@Game/Scritpts/Process/LobbyProcess.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class LobbyProcess : Process
 {
+    public CinemachineCamera _camera;
     public List<SpawnConfig> _spawnConfig;
 
     public void OnEnable()
     {
-        UnitFactory.Instance.CreateUnit(_spawnConfig[0].unit, _spawnConfig[0].point.position, null, Team.Friendly);
+        var player = UnitFactory.Instance.CreateUnit(_spawnConfig[0].unit, _spawnConfig[0].point.position, null, Team.Friendly);
+        _camera.Follow = player.transform;
         UnitFactory.Instance.CreateUnit(_spawnConfig[1].unit, _spawnConfig[1].point.position, null, Team.Enemy);
     }
 
