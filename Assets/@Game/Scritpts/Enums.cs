f4bb02552bbd1010803623bc8f8a884f5374d65e
Thon <wooson0308@gmail.com>
2025-01-23 19:09:37 +0900

    주문 이름 변경

--- Assets/@Game/Scritpts/Enums.cs
+++ Assets/@Game/Scritpts/Enums.cs
@@ -1,14 +1,12 @@
-using UnityEngine;
-
 public enum Cast
 {
-    FIRE,
-    ICE,
-    EARTH,
-    AIR,
-    WATER,
-    WEAPON,
-    ENCHENT,
-    ARROW,
-    SHIELD,
+    IGNIS,
+    GELUM,
+    TERRA,
+    AER,
+    AQUA,
+    ARMA,
+    INCANTARE,
+    SAGITTA,
+    SCUTUM
 }
